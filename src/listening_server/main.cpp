a68f6088f393211c4c6068c5ce270b0dd15c68d9
Tom <thomas_lockie@yahoo.com>
2024-06-22 14:09:55 +0100

    Push changes for working on Mac

--- src/listening_server/main.cpp
+++ src/listening_server/main.cpp
@@ -1,12 +1,12 @@
+#include <iostream>
 #include <span>
 #include <string>
-#include <vector>
-
-// #include "http_server.h"
 
 #include "oatpp/network/Server.hpp"
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
 #include "oatpp/web/server/HttpConnectionHandler.hpp"
+
+#include "spdlog/spdlog.h"
 
 class Handler : public oatpp::web::server::HttpRequestHandler {
 public:
@@ -21,23 +21,21 @@ void run() {
   auto router = oatpp::web::server::HttpRouter::createShared();
   router->route("GET", "/hello", std::make_shared<Handler>());
 
-  // create HTTP connectio handler with router
+  // create HTTP connection handler with router
   auto connection_handler =
     oatpp::web::server::HttpConnectionHandler::createShared(router);
 
   // tcp connection provider
-  auto connection_provider =
+  const uint8_t port = 80;
+  auto          connection_provider =
     oatpp::network::tcp::server::ConnectionProvider::createShared(
-      {"localhost", 80, oatpp::network::Address::IP_4}
+      {"localhost", port, oatpp::network::Address::IP_4}
     );
 
   oatpp::network::Server server(connection_provider, connection_handler);
 
-  OATPP_LOGI(
-    "MyApp",
-    "Server Port %s",
-    connection_provider->getProperty("port").getData()
-  );
+  std::cout << "Logging was here\n";
+  spdlog::info("Server starting on port {}", 80);
 
   server.run();
 }
@@ -51,17 +49,3 @@ auto main() -> int {
 
   return 0;
 }
-
-// auto main(int argc, const char* argv[]) -> int {
-
-//   const auto span_argv = std::span(argv, argc);
-
-//   std::vector<std::string> args(argc);
-
-//   for (const auto& elem : span_argv) {
-//     args.emplace_back(elem);
-//   }
-
-//   ListeningServer::HttpServer app;
-//   return app.run(args);
-// }
